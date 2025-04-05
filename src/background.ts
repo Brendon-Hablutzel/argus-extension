01333ca91e092d014095c4d7475846158c27c2de
Brendon Hablutzel <booklvrbren@gmail.com>
2025-04-05 15:13:44 -0400

    functionality for sending events to backend

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,7 @@ import { getEndpoint } from './storage'
 
 console.log('background script loaded')
 
-export const getFocusedTabs = async () => {
+const getFocusedTabs = async () => {
   const windows = await chrome.windows.getAll()
   const focusedWindow = windows.find((window) => window.focused)
 
@@ -17,6 +17,32 @@ export const getFocusedTabs = async () => {
   return focusedTabs
 }
 
+interface ActiveTab {
+  timestamp: number
+  url: string
+  title: string
+  status: 'loading' | 'unloaded' | 'complete'
+}
+
+const recordActiveTab = async (
+  endpoint: string,
+  tab: ActiveTab | undefined
+) => {
+  const res = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      tab,
+    }),
+  })
+
+  const data = await res.json()
+
+  return data
+}
+
 setInterval(async () => {
   const endpoint = await getEndpoint()
   console.log('endpoint:', endpoint)
@@ -26,5 +52,33 @@ setInterval(async () => {
   }
 
   const focusedTabs = await getFocusedTabs()
-  console.log('recording focused tabs:', focusedTabs)
+
+  const activeTab = focusedTabs[0] as chrome.tabs.Tab | undefined
+  console.log('recording active tab:', activeTab)
+
+  if (
+    activeTab &&
+    activeTab.status !== 'loading' &&
+    activeTab.status !== 'unloaded' &&
+    activeTab.status !== 'complete'
+  ) {
+    console.error('invalid tab status:', activeTab.status)
+    return
+  }
+
+  const now = new Date()
+
+  const res = await recordActiveTab(
+    endpoint,
+    activeTab
+      ? {
+          timestamp: now.getTime(),
+          url: activeTab.url ?? '',
+          title: activeTab.title ?? '',
+          // must be one of theses becaues of prior check
+          status: activeTab.status as 'loading' | 'unloaded' | 'complete',
+        }
+      : undefined
+  )
+  console.log(res)
 }, 3000)
